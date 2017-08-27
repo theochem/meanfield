0cd24a64ddbe87465ca34ef0d9960b699d3a0230
Matthew Chan <chanmhy@mcmaster.ca>
2017-08-27 13:58:30 +0200

    added install files

---
+++ README.rst
@@ -0,0 +1,13 @@
+.. image:: https://travis-ci.org/theochem/python-cython-ci-example.svg?branch=master
+    :target: https://travis-ci.org/theochem/python-cython-ci-example
+.. image:: https://ci.appveyor.com/api/projects/status/ora4yr7kot2ffr8x/branch/master?svg=true
+    :target: https://ci.appveyor.com/project/theochem-travis-uploader/python-cython-ci-example
+.. image:: https://anaconda.org/theochem/pycydemo/badges/version.svg
+    :target: https://anaconda.org/theochem/pycydemo
+.. image:: https://codecov.io/gh/theochem/python-cython-ci-example/branch/master/graph/badge.svg
+    :target: https://codecov.io/gh/theochem/python-cython-ci-example
+
+HORTON: *H*elpful *O*pen-source *R*esearch *TO*ol for *N*-fermion systems.
+Copyright (C) 2011-2016 The HORTON Development Team
+
+A module from the HORTON project containing various SCF routines and also code for HF/DFT.
---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import numpy as np
+from setuptools import setup, Extension
+import Cython.Build
+
+from tools.gitversion import get_gitversion
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='meanfield',
+    version=get_gitversion('meanfield', verbose=(__name__=='__main__')),
+    description='HORTON module for SCF and HF/DFT methods',
+    long_description=readme(),
+    author='Toon Verstraelen',
+    author_email='Toon.Verstraelen@UGent.be',
+    url='https://github.com/theochem/meanfield',
+    cmdclass={'build_ext': Cython.Build.build_ext},
+    package_dir = {'meanfield': 'meanfield'},
+    packages=['meanfield', 'meanfield.test'],
+    ext_modules=[Extension(
+        'meanfield.cext',
+        sources=['meanfield/cext.pyx'],
+        include_dirs=[np.get_include()],
+    )],
+    zip_safe=False,
+    setup_requires=['numpy>=1.0', 'cython>=0.24.1'],
+    install_requires=['numpy>=1.0', 'nose>=0.11', 'cython>=0.24.1'],
+)
